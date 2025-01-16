c986c9dd57ae2ed46a3d8f9dd802c73f50ff7817
Nasma2001 <nasoma11125@gmail.com>
2025-01-16 18:01:32 +0200

    first commit

---
+++ LeetCodeProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        List<int> sol = new();
+        for (int i = 0; i < nums.Length; i++)
+        {
+           for (int j = i+1; j < nums.Length ; j++){
+            if (nums[i]+nums[j] == target){
+                sol.Add(i);
+                sol.Add(j);
+           }}
+        }
+        return sol.ToArray();
+    }
+}
